68f232c78400900cefd3f9b1e3e5d83b3c5fe31f
Meghana A <meghanaa1315@gmail.com>
2025-04-18 16:25:34 +0530

    Initial Commit

---
+++ JavaScript/Classlist.js
@@ -0,0 +1,39 @@
+// let box = document.getElementById('box')
+
+// let redcolor = ()=>{
+//     box.classList.add('red')
+//     box.classList.remove('green')
+
+// }
+
+// let greencolor=()=>{
+//     box.classList.add('green')
+//     box.classList.remove('red')
+
+// }
+
+
+//if 3 colors tn do below
+
+let box = document.getElementById('box')
+
+let redcolor = ()=>{
+    box.classList.add('red')
+    box.classList.remove('green')
+    box.classList.remove('yellow')
+}
+
+let greencolor=()=>{
+    box.classList.add('green')
+    box.classList.remove('red')
+    box.classList.remove('yellow')
+
+
+}
+let yellowcolor=()=>{
+    box.classList.add('yellow')
+    box.classList.remove('red')
+    box.classList.remove('green')
+
+
+}
