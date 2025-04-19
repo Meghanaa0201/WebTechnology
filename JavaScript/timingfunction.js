e617722027eb6e599a97381c2a52dae061d38aed
Meghana A <meghanaa1315@gmail.com>
2025-04-19 12:40:39 +0530

    Initial Commit

--- JavaScript/timingfunction.js
+++ JavaScript/timingfunction.js
@@ -7,3 +7,6 @@ setTimeout(()=>{
 
 } , 2000)
 
+// in fun() 1st line it is used for calling the function for html  but in html we add 2000 for next method u can just call fun()
+
+//instead of agaain calling the function
